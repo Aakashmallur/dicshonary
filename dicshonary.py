1d5078a3c21c6814c03210bd74b05cd2b94695f8
Aakashmallur <aakashmallur@gmail.com>
2024-10-25 17:02:52 -0400

    Add files via upload

---
+++ dicshonary.py
@@ -0,0 +1,19 @@
+#how to crate a dicshnary
+person={"Name" : "aakash" , "age" : 11 , "year" :4, }
+print(person)
+
+print(person.keys())
+
+print(person.values())
+
+#how to add new item
+
+person["hobby"] = "robitcs"
+
+
+print(person)
+
+#access any vale form dicshnary
+
+
+print(person["Name"])
